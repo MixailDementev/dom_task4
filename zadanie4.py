b653b34e1265d3b59e6e2b657013ded4e79a21e8
MDementev <vanneste1207@gmail.com>
2022-10-22 18:16:26 +0300

    4 задача

---
+++ zadanie4.py
@@ -0,0 +1,62 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от -100 до 100)
+# многочлена и записать в файл многочлен степени k
+# k - максимальная степень многочлена, следующий степень следующего на 1 меньше и так до ноля
+# Коэффициенты расставляет random, поэтому при коэффициенте 0 просто пропускаем данную итерацию степени
+# Записываем результат в файл.
+# Пример:
+# k=2 -> 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10x² = 0
+# k=5 -> 3x⁵ + 5x⁴ - 6x³ - 3x = 0
+
+import random
+
+
+def w_file(poly):
+    with open('polynomial.txt', 'w') as data:
+        data.write(poly)
+
+
+def rnd():
+    return random.randint(-100, 100)
+
+
+def degree(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+
+
+def polynomial(sp):
+    lst = sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    if lst[i+1] > 0:
+                        wr += '+'
+                    else:
+                        wr += ' '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    if lst[i+1] > 0:
+                        wr += '+'
+                    else:
+                        wr += ' '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+
+k = int(input("Введите натуральную степень k = "))
+koeff = degree(k)
+w_file(polynomial(koeff))
+
+data = open('polynomial.txt', 'r')
+for line in data:
+    print(line)
+data.close()
