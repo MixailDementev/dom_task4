e8a3daa804dbf919c40b05e9fe4fc5c79bc1d9a1
MDementev <vanneste1207@gmail.com>
2022-10-22 19:28:30 +0300

    объединил 4 и 5  задачи

--- zadanie4.py
+++ zadanie4.py
@@ -1,3 +1,4 @@
+# Задача 4+5
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от -100 до 100)
 # многочлена и записать в файл многочлен степени k
 # k - максимальная степень многочлена, следующий степень следующего на 1 меньше и так до ноля
@@ -7,25 +8,35 @@
 # k=2 -> 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10x² = 0
 # k=5 -> 3x⁵ + 5x⁴ - 6x³ - 3x = 0
 
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+# Пример двух заданных многочленов:
+# 23x⁹ - 16x⁸ + 3x⁷ + 15x⁴ - 2x³ + x² + 20 = 0
+# 17x⁹ + 15x⁸ - 8x⁷ + 15x⁶ - 10x⁴ + 7x³ - 13x + 33 = 0
+# Результат:
+# 40x⁹ - x⁸ -5x⁷ + 15x⁶ +5x⁴ + 5x³ + x² - 13x + 53 = 0
+
 import random
 
-
-def w_file(poly):
-    with open('polynomial.txt', 'w') as data:
-        data.write(poly)
-
-
+# запись в файл
+def w_file(name,st):
+    with open(name, 'w') as data:
+        data.write(st)
+        
+# создание случайного числа от 0 до 100
 def rnd():
-    return random.randint(-100, 100)
+    return random.randint(0,101)
 
 
-def degree(k):
+# создание коэффициентов многочлена
+def koeff(k):
     lst = [rnd() for i in range(k+1)]
     return lst
+    
 
-
-def polynomial(sp):
-    lst = sp[::-1]
+# создание многочлена в виде строки 
+def polynomial_str(sp):
+    lst= sp[::-1]
     wr = ''
     if len(lst) < 1:
         wr = 'x = 0'
@@ -33,18 +44,12 @@ def polynomial(sp):
         for i in range(len(lst)):
             if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
                 wr += f'{lst[i]}x^{len(lst)-i-1}'
-                if lst[i+1] != 0:
-                    if lst[i+1] > 0:
-                        wr += '+'
-                    else:
-                        wr += ' '
+                if lst[i+1] != 0 or lst[i+2] != 0:
+                    wr += ' + '
             elif i == len(lst) - 2 and lst[i] != 0:
                 wr += f'{lst[i]}x'
-                if lst[i+1] != 0:
-                    if lst[i+1] > 0:
-                        wr += '+'
-                    else:
-                        wr += ' '
+                if lst[i+1] != 0 or lst[i+2] != 0:
+                    wr += ' + '
             elif i == len(lst) - 1 and lst[i] != 0:
                 wr += f'{lst[i]} = 0'
             elif i == len(lst) - 1 and lst[i] == 0:
@@ -52,11 +57,81 @@ def polynomial(sp):
     return wr
 
 
-k = int(input("Введите натуральную степень k = "))
-koeff = degree(k)
-w_file(polynomial(koeff))
+# получение степени многочлена
+def degree_polynomial(k):
+    if 'x^' in k:
+        i = k.find('^')
+        num = int(k[i+1:])
+    elif ('x' in k) and ('^' not in k):
+        num = 1
+    else:
+        num = -1
+    return num
 
-data = open('polynomial.txt', 'r')
-for line in data:
-    print(line)
-data.close()
+# получение коэффицента члена многочлена
+def koeff_polynomial(k):
+    if 'x' in k:
+        i = k.find('x')
+        num = int(k[:i])
+    return num
+
+# разбор многочлена и получение его коэффициентов
+def calc_polynomial(st):
+    st = st[0].replace(' ', '').split('=')
+    st = st[0].split('+')
+    lst = []
+    l = len(st)
+    k = 0
+    if degree_polynomial(st[-1]) == -1:
+        lst.append(int(st[-1]))
+        l -= 1
+        k = 1
+    i = 1 # степень
+    ii = l-1 # индекс
+    while ii >= 0:
+        if degree_polynomial(st[ii]) != -1 and degree_polynomial(st[ii]) == i:
+            lst.append(koeff_polynomial(st[ii]))
+            ii -= 1
+            i += 1
+        else:
+            lst.append(0)
+            i += 1
+        
+    return lst
+    
+# создание двух файлов
+k1 = int(input("Введите натуральную степень для первого файла k = "))
+k2 = int(input("Введите натуральную степень для второго файла k = "))
+koeff1 = koeff(k1)
+koeff2 = koeff(k2)
+w_file('polynomial1.txt', polynomial_str(koeff1))
+w_file('polynomial2.txt', polynomial_str(koeff2))
+
+
+# нахождение суммы многочлена
+with open('polynomial1.txt', 'r') as data:
+    poly1 = data.readlines()
+with open('polynomial2.txt', 'r') as data:
+    poly2 = data.readlines()
+print(f"Первый многочлен {poly1}")
+print(f"Второй многочлен {poly2}")
+
+
+lst1 = calc_polynomial(poly1)
+lst2 = calc_polynomial(poly2)
+ll = len(lst1)
+if len(lst1) > len(lst2):
+    ll = len(lst2)
+lst_new = [lst1[i] + lst2[i] for i in range(ll)]
+if len(lst1) > len(lst2):
+    mm = len(lst1)
+    for i in range(ll,mm):
+        lst_new.append(lst1[i])
+else:
+    mm = len(lst2)
+    for i in range(ll,mm):
+        lst_new.append(lst2[i])
+w_file("polynomial3.txt", polynomial_str(lst_new))
+with open('polynomial3.txt', 'r') as data:
+    poly3 = data.readlines()
+print(f"Результирующий многочлен {poly3}")
